58721ab39fe95a7ec5a36b06eafa42e75baa9994
alekh-thakur <alekhrathor8@gmail.com>
2026-03-11 23:01:46 +0530

    Fixed minor issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,14 +69,14 @@ const Navbar = ({ dark, setDark }) => {
           {dark ? <RiSunFill /> : <RiMoonFill />}
         </button>
 
-        {["Home","About","Skills","Projects","Contact"].map((item) => (
+        {["home","about","skills","projects","contact"].map((item) => (
           <li key={item}>
             <a
               href={`#${item}`}
               className={navStyle(item)}
               onClick={() => handleNavClick(item)}
             >
-              {item}
+              {item.charAt(0).toUpperCase() + item.slice(1)}
             </a>
           </li>
         ))}
@@ -104,14 +104,14 @@ const Navbar = ({ dark, setDark }) => {
           {dark ? <RiSunFill /> : <RiMoonFill />}
         </button>
 
-        {["Home","About","Skills","Projects","Contact"].map((item) => (
+        {["home","about","skills","projects","contact"].map((item) => (
           <a
             key={item}
             href={`#${item}`}
             className={navStyle(item)}
             onClick={() => handleNavClick(item)}
           >
-            {item}
+            {item.charAt(0).toUpperCase() + item.slice(1)}
           </a>
         ))}
       </div>
